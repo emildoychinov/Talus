b85e1799bb1aa24833b7c618d003752e57ab1351
Emil Doychinov <65024936+emildoychinov@users.noreply.github.com>
2021-05-22 22:15:46 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -7,9 +7,8 @@ class misc (commands.Cog):
         self.bot = bot
     @commands.command()
     async def solve(self, ctx, *, arg):
-        #print(arg)
-        solver = maths.parser(arg)
-        embed=discord.Embed(title = arg, description=("The equation equals "+str(solver.evaluate())))
+        solver = maths.parser()
+        embed=discord.Embed(title = solver.analyze(arg), description=("The equation equals "+str(solver.evaluate(arg))))
         await ctx.channel.send(embed=embed)
 
 def setup(bot):
