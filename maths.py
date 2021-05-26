4b2606100a37ced7e00222caafac14164947fbf6
Emil Doychinov <65024936+emildoychinov@users.noreply.github.com>
2021-05-26 12:38:29 +0300

    Yet another parser commit...
    Yes, I did say that it's finally ready to go but a problem with the brackets and raising a number to a power popped up and it had to be solved

--- maths.py
+++ maths.py
@@ -18,6 +18,10 @@ class parser :
         self.operators = []
         self.operands = []
 
+    def split(self, expr) :
+        expr = re.split(r'([\s\+\-\*\/\^\(\)])', expr)
+        return list(filter(lambda x: x!='' and x!=' ', expr))
+
     def calc(self, operator, operand1, operand2):
         if operator == '+' : return operand1 + operand2
         elif operator == '-' : return operand1 - operand2
@@ -38,14 +42,15 @@ class parser :
         self.expr = self.analyze(expr)
         self.operators.clear()
         self.operands.clear()
+        self.expr = self.split(self.expr)
         for i in range(len(self.expr)) :
-            if self.expr[i] == '-' and self.expr[i-1] not in self.tokens : self.operators.append('+')
+            if self.expr[i] == '-' and self.expr[i-1] not in self.tokens : self.operators.append('-' if self.expr[i+2 if i+2 in range(len(self.expr)) else i+1] == '^' else '+')
             elif self.expr[i] in self.tokens and self.tokens[self.expr[i]]!=TOKEN.PAR and self.expr[i] != '-' : self.operators.append(self.expr[i])
+        self.expr = ''.join(list(self.expr))
         self.operands = re.findall(r'\-?\d+\.\d+|\-?\d+', self.expr)
 
     def analyze(self, expr):
-        expression = re.split(r'([\s\+\-\*\/\^\(\)])', expr)
-        expression = list(filter(lambda x: x!='' and x!=' ', expression))
+        expression = self.split(expr)
         i = 0
         while i < len(expression):
             count = 0
@@ -58,21 +63,20 @@ class parser :
                     expression.insert(i, '+' if count % 2 == 0 and count!=0 else '-')
 
                 elif self.tokens[expression[i]]!=TOKEN.PAR and expression[i]!='-':
-                    while expression[i] == expression[i+1] :
-                        expression.pop(i)
+                    while expression[i] == expression[i+1] : expression.pop(i)
 
                 elif expression[i] == '(' :
                     par = ''
-                    count = 0
-                    expression.pop(i)
+                    ix = i
                     while expression[i]!=')':
-                        par+=expression[i]
+                        i+=1
                         if(expression[i] == '('):
-                            count+=1
-                        expression.pop(i)
-                    expression.pop(i)
-                    for j in range(count) : par+=')'
-                    expression.insert(i,self.evaluate(par))
+                            ix = i
+                    expression.pop(ix)
+                    while expression[ix]!=')':
+                        par+=expression.pop(ix)
+                    expression.pop(ix)
+                    expression.insert(ix, self.evaluate(par))
                     i = 0
             i+=1
         return ''.join(list(expression))
@@ -82,7 +86,5 @@ class parser :
         self.lex(self.expr)
         for i in range(len(self.operators)):
             ix = self.get_precedent()
-            self.operands[ix] = str(self.calc(self.operators[ix], float(self.operands[ix]), float(self.operands[ix+1])))
-            self.operands.pop(ix+1)
-            self.operators.pop(ix)
+            self.operands[ix] = str(self.calc(self.operators.pop(ix), float(self.operands[ix]), float(self.operands.pop(ix+1))))
         return self.operands[0]
