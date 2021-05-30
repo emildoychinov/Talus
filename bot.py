041f874666b9dc058fdbb3ca8d2af4a3351fe01e
Emil Doychinov <65024936+emildoychinov@users.noreply.github.com>
2021-05-30 17:55:24 +0300

    Update bot.py
    A few changes to the script where the bot is run from. They were made in order for the chat command and the other commands to work

--- bot.py
+++ bot.py
@@ -1,7 +1,17 @@
 from discord.ext import commands
+import chat
 bot = commands.Bot(command_prefix='~')
 bot.load_extension('ttt')
 bot.load_extension('imagery')
 bot.load_extension('utils')
 bot.load_extension('misc')
+chatbot = chat.chat()
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    print(message.content)
+    if str(bot.user.id) in message.content:
+        msg = message.content.replace(f'<@!{bot.user.id}> ' if f'<@!{bot.user.id}>' in message.content else '@{bot.user.id}> ','')
+        print(msg)
+        await message.channel.send(chatbot.respond(message.content))
 bot.run('mytoken')
