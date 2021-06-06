488d7f20524bc6fd76a60726f5ceffcf10e12221
Emil Doychinov <65024936+emildoychinov@users.noreply.github.com>
2021-06-07 02:55:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ chatter.prepare()
 @bot.group(invoke_without_command = True)
 async def help(ctx):
     embed = discord.Embed(title = "Help", description = "`Use ~help <command> for extended info`")
-    embed.add_field(name = "Miscellaneous", value = "```solve, game```")
-    embed.add_field(name = "Imagery :art:", value = "```modify, rotate```")
-    embed.add_field(name = "Moderation :police_officer:", value = "```clear```")
+    embed.add_field(name = "Miscellaneous", value = "```solve, game, avatar```")
+    embed.add_field(name = "Imagery :art:", value = "```modify, rotate, invert```")
+    embed.add_field(name = "Moderation :police_officer:", value = "```clear, ban```")
     await ctx.send(embed = embed)
 @help.command()
 async def solve(ctx):
@@ -33,9 +33,15 @@ async def rotate(ctx):
     embed.add_field(name = "Use", value = "```~rotate <number> <image>```")
     await ctx.send(embed = embed)
 @help.command()
+async def invert(ctx):
+    embed = discord.Embed(title = "Invert :art:", description = "`Inverts a provided image`")
+    embed.add_field(name = "Use", value = "```~invert <image>```")
+    await ctx.send(embed = embed)
+@help.command()
 async def clear(ctx):
     embed = discord.Embed(title = "Clear :pencil2:", description = "`Clears a provided number of messages`")
     embed.add_field(name = "Use", value = "```~clear <number>```")
+    embed.add_field(name = "Note :exclamation:", value = "```You cannot use that command unless you have the permisions to manage guilds```")
     await ctx.send(embed = embed)
 @help.command()
 async def game(ctx):
@@ -44,11 +50,23 @@ async def game(ctx):
     embed.add_field(name = "Stop the game", value = "```Type stop to stop the game```")
     embed.add_field(name = "Playing the game", value = "```While playing the game, type in a number between 1 and 9 to take up a position on the field```")
     await ctx.send(embed = embed)
+@help.command()
+async def avatar(ctx):
+    embed = discord.Embed(title = "Avatar :person_doing_cartwheel:", description = "`Sends the avatar of a tagged user`")
+    embed.add_field(name = "Use", value = "```~avatar <user>```")
+    embed.add_field(name = "Note :exclamation:", value = "```If a user is not mentioned your avatar will be sent```")
+    await ctx.send(embed = embed)
+@help.command()
+async def ban(ctx):
+    embed = discord.Embed(title = "Ban :police_car:", description = "`Bans a tagged user`")
+    embed.add_field(name = "Use", value = "```~ban <user> <reason>```")
+    embed.add_field(name = "Note :exclamation:", value = "```You cannot use that command unless you have the permisions to kick people```")
+    await ctx.send(embed = embed)
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
     print(message.content)
-    if str(bot.user.id) in message.content:
+    if str(bot.user.id) in message.content and message.content[0]!='~':
         msg = message.content.replace(f'<@!{bot.user.id}> ' if f'<@!{bot.user.id}>' in message.content else '@{bot.user.id}> ','')
         await message.channel.send(chatter.respond(msg))
-bot.run('mytoken')
+bot.run('ODE0NTkwMzQzMzY1Mzk0NDUy.YDgEYA.HDsHkV9FSKSFYKU9_oyqZPXkg_A')
