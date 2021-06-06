c8858412936a3cf1b4291f9e2ce8bad6ec492bf7
Emil Doychinov <65024936+emildoychinov@users.noreply.github.com>
2021-06-07 02:56:57 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -10,6 +10,14 @@ class misc (commands.Cog):
         solver = maths.parser(arg)
         embed=discord.Embed(title = arg, description=("The equation equals " + str(solver.term())))
         await ctx.channel.send(embed=embed)
+    @commands.command()
+    async def avatar(self, ctx, member : discord.Member = None):
+        if member == None : member = ctx.message.author
+        avatarUrl = member.avatar_url
+        embed = discord.Embed(title = f"```{member}'s avatar```")
+        embed.set_image(url = avatarUrl)
+        await ctx.send(embed = embed)
+        return
 
 def setup(bot):
     bot.add_cog(misc(bot))
