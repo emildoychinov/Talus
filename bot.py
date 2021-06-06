ea448f308cf33463621285535082d564ef3aad18
Emil Doychinov <65024936+emildoychinov@users.noreply.github.com>
2021-06-06 16:14:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 from discord.ext import commands
 import chat
+from chatbot import chatter
 bot = commands.Bot(command_prefix='~')
 bot.load_extension('ttt')
 bot.load_extension('imagery')
 bot.load_extension('utils')
 bot.load_extension('misc')
-chatbot = chat.chat()
+chatter = chatter.ChatBot()
+chatter.prepare()
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
     print(message.content)
     if str(bot.user.id) in message.content:
         msg = message.content.replace(f'<@!{bot.user.id}> ' if f'<@!{bot.user.id}>' in message.content else '@{bot.user.id}> ','')
-        print(msg)
-        await message.channel.send(chatbot.respond(message.content))
+        await message.channel.send(chatter.respond(msg))
 bot.run('mytoken')
